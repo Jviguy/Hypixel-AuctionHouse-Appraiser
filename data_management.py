cf7fd9f2424af844e8e3d0e3aac51c52c371f0db
Jviguy <jviguytwo2@gmail.com>
2024-01-03 01:33:16 -0500

    3.76 mae on cross val!!!!!

--- data_management.py
+++ data_management.py
@@ -1,12 +1,15 @@
 import base64
+import datetime
 import gzip
+import json
 import os
+import sys
 from io import BytesIO
 from nbt import nbt
 import pandas as pd
 from nbt.nbt import TAG_String
 
-from auction_endpoint_stuff import get_all_bins
+from auction_endpoint_stuff import get_all_bins, get_auctions
 
 
 def auction_to_dataframe(auction: dict) -> pd.DataFrame:
@@ -18,8 +21,13 @@ def auction_to_dataframe(auction: dict) -> pd.DataFrame:
     item_bytes = auction['item_bytes']
     nbtf = item_bytes_to_nbt(item_bytes)
     tag = nbtf['i'][0]['tag']['ExtraAttributes']
+    pet_info = get_pet_info_from_nbt(tag)
+    rgb = get_rgb_from_nbt(tag)
+    start_time = unix_to_skyblock_time(auction['start'])
+    end_time = unix_to_skyblock_time(auction['end'])
     data = [{
         'auction_id': auction['uuid'],
+        'item_count': get_item_count_from_nbt(nbtf['i'][0]),
         'item_id': get_item_name_from_nbt(tag),
         'price': auction['starting_bid'],
         'tier': auction['tier'],
@@ -31,12 +39,41 @@ def auction_to_dataframe(auction: dict) -> pd.DataFrame:
         'recomb': recomb_from_nbt(tag),
         'unlocked_gem_slots': get_unlocked_gem_slots_from_nbt(tag),
         'slotted_gems': get_slotted_gems_from_nbt(tag),
+        'pet_type': pet_info['type'],
+        'pet_active': pet_info['active'],
+        'pet_held_item': pet_info['heldItem'],
+        'pet_exp': pet_info['exp'],
+        'pet_candy_used': pet_info['candyUsed'],
+        'dungeon_item_level': get_dungeon_item_level_from_nbt(tag),
+        'red_armor_coloring': rgb[0],
+        'green_armor_coloring': rgb[1],
+        'blue_armor_coloring': rgb[2],
+        'anvil_uses': get_anvil_uses_from_nbt(tag),
+        'pelts_earned': get_pelts_earned_from_nbt(tag),
+        'champion_combat_xp': get_champion_combat_xp_from_nbt(tag),
+        'farmed_cultivating': get_farmed_cultivating_from_nbt(tag),
+        'compact_blocks': get_compact_blocks_from_nbt(tag),
+        'hecatomb_s_runs': get_hecatomb_s_runs_from_nbt(tag),
+        'expertise_kills': get_expertise_kills_from_nbt(tag),
+        'runes': get_runes_from_nbt(tag),
+        'start_day': start_time['day'],
+        'start_month': start_time['month'],
+        'start_year': start_time['year'],
+        'end_day': end_time['day'],
+        'end_month': end_time['month'],
+        'end_year': end_time['year']
     }]
     return pd.DataFrame(data)
 
 
 def item_bytes_to_nbt(b: str) -> nbt.NBTFile:
     return nbt.NBTFile(buffer=BytesIO(gzip.decompress(base64.b64decode(b))))
+
+
+def get_item_count_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'Count' not in t:
+        return -1
+    return t['Count'].value
 
 
 def get_enchantments_from_nbt(t: nbt.TAG_COMPOUND) -> list:
@@ -105,16 +142,126 @@ def get_slotted_gems_from_nbt(t: nbt.TAG_COMPOUND) -> list:
     return li
 
 
-x = get_all_bins()
-# Check if the file exists
-if os.path.isfile("auctions.csv"):
-    all_data = pd.read_csv("auctions.csv")
+def get_pet_info_from_nbt(t: nbt.TAG_COMPOUND) -> dict:
+    if 'petInfo' not in t:
+        return {"type": "", "exp": -1, "candyUsed": -1, "active": False, "heldItem": "", "tier": ""}
+    t = json.loads(t['petInfo'].value)
+    ret = {}
+    if 'type' in t:
+        ret['type'] = t['type']
+    else:
+        ret['type'] = ""
+    if 'exp' in t:
+        ret['exp'] = t['exp']
+    else:
+        ret['exp'] = -1
+    if 'candyUsed' in t:
+        ret['candyUsed'] = t['candyUsed']
+    else:
+        ret['candyUsed'] = -1
+    if 'active' in t:
+        ret['active'] = t['active']
+    else:
+        ret['active'] = False
+    if 'heldItem' in t:
+        ret['heldItem'] = t['heldItem']
+    else:
+        ret['heldItem'] = ""
+    if 'tier' in t:
+        ret['tier'] = t['tier']
+    else:
+        ret['tier'] = ""
+    return ret
+
+
+def get_dungeon_item_level_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'dungeon_item_level' not in t:
+        return -1
+    return t['dungeon_item_level'].value
+
+
+def get_rgb_from_nbt(t: nbt.TAG_COMPOUND) -> tuple:
+    if 'color' not in t:
+        return -1, -1, -1
+    t = t['color'].value
+    t = list(map(int, t.split(":")))
+    return t[0], t[1], t[2]
+
+
+def get_anvil_uses_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'anvil_uses' not in t:
+        return -1
+    return t['anvil_uses'].value
+
+
+def get_pelts_earned_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'pelts_earned' not in t:
+        return -1
+    return t['pelts_earned'].value
+
+
+def get_champion_combat_xp_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'champion_combat_xp' not in t:
+        return -1
+    return t['champion_combat_xp'].value
+
+
+def get_farmed_cultivating_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'farmed_cultivating' not in t:
+        return -1
+    return t['farmed_cultivating'].value
+
+
+def get_compact_blocks_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'compact_blocks' not in t:
+        return -1
+    return t['compact_blocks'].value
+
+
+def get_hecatomb_s_runs_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'hecatomb_s_runs' not in t:
+        return -1
+    return t['hecatomb_s_runs'].value
+
+
+def get_expertise_kills_from_nbt(t: nbt.TAG_COMPOUND) -> int:
+    if 'expertise_kills' not in t:
+        return -1
+    return t['expertise_kills'].value
+
+
+def get_runes_from_nbt(t: nbt.TAG_COMPOUND) -> list:
+    if 'runes' not in t:
+        return []
+    li = []
+    for ta in t['runes']:
+        li.append(ta)
+    return li
+
+
+def unix_to_skyblock_time(unix_timestamp):
+    d = datetime.datetime.fromtimestamp(unix_timestamp / 1000.0)
+    return {'day': d.day, 'month': d.month, 'year': d.year}
+
+
+inp = input("Gather all data? (y/n)")
+inp.strip()
+inp = inp.lower()
+if inp == "n":
+    nb = item_bytes_to_nbt(
+        "H4sIAAAAAAAAAFWS0Y6aQBSGR3e3VdO06ROUJpv0ygZ12e1eUhAZd2EqIgo3zQAjDAxCAHXxHfoKvfU9fLCm426bpleT/Oc75/9P5vQA6IIW7QEAWm3QpmHrRwtcKfl2U7d74KLG0QXo6jQkGsNRxalfPdCbp1vG0H5Dyg5owxBc3wZYDIZh2L8T/S/90ehW6t/7GPcDCUv3oXQ3JCLhfd/KvCBlTUnVBZ2aPNXbklTP1h1w5WC2Ja2fZJ9HUJmKeDlgwciK/ZVMoZpHhu0OzUN0MBJ3hOzx3mz2D1CRaaBPd17GKm/BUkjlW6jAkaHORE9lqTuEEppA0eV97lJLzAQezERjru1KbmZmaA4rhcoR3Hxt/KFX+BMHudz3Zc507Qw1MdiwA57U8ar5y5qFN5TiUHcaz5myYOUUQea8eOtWEy4XfziLEd0a8NrhpVad8553s+ciQ/9rZ95pfAVGiMoU65YYqPnucfRvxmM2KPzMSYJMy0JF2nqr2S6cODfPOeb31FDTkafKN+7SitFkmnp8RyOBN8ZhNnATLzVseWAmToLUheglYezZLDWSmeja4wapLDOW0wwt3cazIxHZs4FpR3sY5c/Z1jP+6uLDesb/qgdeh7QqGG664PIxL0mHi1fg+nS8m9clplFcC+syz4Q6JkJV8NMpP1VClm/r+DMnP5yOxDpD/YDRIBXqXNhRshdKEtCCVB858vZ0xKcjW5gKMgxkdsCliTMC3nNZLnEQbwgfqOFNxLO8Gz9xU7muS+pva1J1zjcM3siWrOjm+LsmmxMA2uCVijMcEXABwG+yoS4H8gIAAA\u003d\u003d")
+    print(nb.pretty_tree())
 else:
-    all_data = pd.DataFrame()  # Create an empty DataFrame if the file doesn't exist
+    x = get_all_bins()
+    # Check if the file exists
+    if os.path.isfile("auctions.csv"):
+        all_data = pd.read_csv("auctions.csv")
+    else:
+        all_data = pd.DataFrame()  # Create an empty DataFrame if the file doesn't exist
 
-# Convert each item in 'x' to a DataFrame and store them in a list
-list_of_df = [auction_to_dataframe(item) for item in x]
+    # Convert each item in 'x' to a DataFrame and store them in a list
+    list_of_df = [auction_to_dataframe(item) for item in x]
 
-# Concatenate all DataFrames
-all_data = pd.concat([all_data] + list_of_df, ignore_index=True)
-all_data.to_csv("auctions.csv", index=False)
+    # Concatenate all DataFrames
+    all_data = pd.concat([all_data] + list_of_df, ignore_index=True)
+    all_data.to_csv("auctions.csv", index=False)
--- model.py
+++ model.py
@@ -6,27 +6,29 @@ from hyperopt import hp, STATUS_OK, Trials, fmin, tpe
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer, OneHotEncoder
-from xgboost import XGBRFRegressor, XGBRegressor
+from xgboost import XGBRegressor, XGBRFRegressor
 
 df = pd.read_csv('auctions.csv')
 
 # create item_count_on_market column
-item_counts = df['item_id'].value_counts()
-# Convert the Series to a DataFrame
-item_counts_df = item_counts.reset_index()
-item_counts_df.columns = ['item_id', 'item_count_on_market']
-df = df.merge(item_counts_df, on='item_id', how='left')
+df["item_count_on_market"] = df.groupby('item_id')['item_count'].transform('sum')
 
 # Fix missing reforges, or empty strings
 df['reforge'] = df['reforge'].fillna('None')
 # create lowest_bin column
 
 # Get the lowest BIN for each item
-lowest_bin = df.groupby('item_id')['price'].min()
-lowest_price_per_item = df.groupby('item_id')['price'].min().reset_index()
-lowest_price_per_item.rename(columns={'price': 'lowest_bin'}, inplace=True)
-df = df.merge(lowest_price_per_item, on='item_id', how='left')
+df['lowest_bin'] = df.groupby('item_id')['price'].transform('min')
+df['median_bin'] = df.groupby('item_id')['price'].transform('median')
 df = df.drop('item_id', axis=1)
+
+# preprocess pet held item
+df['pet_held_item'] = df['pet_held_item'].fillna('No Pet Held Item')
+# turn pet held item into a onehot encoded column
+one_hot_encoder = OneHotEncoder(sparse_output=False)
+pet_held_item_encoded = one_hot_encoder.fit_transform(df[['pet_held_item']])
+df = df.join(pd.DataFrame(pet_held_item_encoded, columns=one_hot_encoder.categories_[0], index=df.index))
+df = df.drop('pet_held_item', axis=1)
 
 # preprocess tier.
 label_encoder = LabelEncoder()
@@ -49,6 +51,16 @@ df.drop('category', axis=1, inplace=True)
 # Join the new DataFrame with the original one
 df = df.join(category_encoded_df)
 
+# preprocess pet type.
+one_hot_encoder = OneHotEncoder(sparse_output=False)
+df['pet_type'] = df['pet_type'].fillna('NA')
+pet_type_encoded = one_hot_encoder.fit_transform(df[['pet_type']])
+df.drop('pet_type', axis=1, inplace=True)
+# Create a DataFrame from the encoded data
+pet_type_encoded_df = pd.DataFrame(pet_type_encoded, columns=one_hot_encoder.categories_[0], index=df.index)
+# Join the new DataFrame with the original one
+df = df.join(pet_type_encoded_df)
+
 # preprocess reforge. (one hot)
 one_hot_encoder = OneHotEncoder(sparse_output=False)
 reforge_encoded = one_hot_encoder.fit_transform(df[['reforge']])
@@ -65,6 +77,14 @@ df['enchantments'] = df['enchantments'].apply(ast.literal_eval)
 df = df.join(pd.DataFrame(mlb.fit_transform(df.pop('enchantments')),
                           columns=mlb.classes_,
                           index=df.index))
+
+# preprocess runes through multilabel bin
+mlb = MultiLabelBinarizer()
+df['runes'] = df['runes'].apply(ast.literal_eval)
+
+df = df.join(pd.DataFrame(mlb.fit_transform(df.pop('runes')),
+                          columns=mlb.classes_,
+                          index=df.index), lsuffix='_runes')
 
 # preprocess unlocked_gem_slots through multilabel bin
 mlb = MultiLabelBinarizer()
@@ -84,8 +104,8 @@ y = df['price']
 X = df.drop(['auction_id', 'price'], axis=1)
 # train test split fn
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# HYPERPARAMETER STUFF
 '''
-HYPERPARAMETER STUFF
 space = {
     'n_estimators': hp.quniform('n_estimators', 100, 1000, 1),
     'learning_rate': hp.uniform('learning_rate', 0.01, 0.2),
@@ -126,13 +146,8 @@ if os.path.isfile("model.json"):
     model = XGBRegressor()
     model.load_model('model.json')
 else:
-    model = XGBRegressor(n_estimators=885, n_jobs=4, max_depth=10, subsample=0.9460876879182103,
-                         colsample_bytree=0.5599455461165574,
-                         learning_rate=0.14585998256990643, min_child_weight=1, gamma=0.21577976888055775)
-# print(-cross_val_score(model, X, y, cv=5, scoring='neg_mean_absolute_error').mean())
-model.fit(X_train, y_train)
-explainer = shap.Explainer(model)
-shap_values = explainer(X_train)
-
-# Visualize the first prediction's explanation
-shap.plots.waterfall(shap_values[0], max_display=800)
+    model = XGBRegressor(colsample_bytree=0.7981807211325997, gamma=0.3971583969930645,
+                         learning_rate=0.1470626010192531,
+                         max_depth=10, min_child_weight=1, n_estimators=823, subsample=0.9714879610003891,
+                         n_jobs=4)
+print(-cross_val_score(model, X, y, cv=5, scoring='neg_mean_absolute_error').mean())
